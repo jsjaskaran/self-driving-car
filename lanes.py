c8763d0eb31818930ebeb8c06319578b3adf5434
Jaskaran <jaskarans13@gmail.com>
2018-11-10 18:18:07 +0530

    finding region of interest

--- lanes.py
+++ lanes.py
@@ -18,12 +18,24 @@ def canny(image):
 
 	return canny
 
+# Region of Interest
+def region_of_interest(image):
+	height = image.shape[0]
+	polygons = np.array([[(200, height), (1100, height), (550, 250)]])
+	mask = np.zeros_like(image)
+	cv2.fillPoly(mask, polygons, 255)
+	
+	# bitwise and(&) with all 1's has no effect
+	masked_image = cv2.bitwise_and(image, mask)
+
+	return masked_image
 
 image = cv2.imread('2.1_test_image.jpg')
 # print (type(image))
 lane_image = np.copy(image)
 canny_image = canny(lane_image)
+cropped_image = region_of_interest(canny_image)
 
 # show image
-cv2.imshow('result', canny_image)
+cv2.imshow('result', cropped_image)
 cv2.waitKey(0)
