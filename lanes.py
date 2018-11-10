d694718e197ad0e4368f4b4e61a51d83f027cc5e
Jaskaran <jaskarans13@gmail.com>
2018-11-10 18:24:34 +0530

    added HoughLines, Average of all lines and smoothening

--- lanes.py
+++ lanes.py
@@ -1,10 +1,7 @@
-# finding lanes on the road
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# canny edge detection
 def canny(image):
 
 	# Convert image to GrayScale
@@ -13,7 +10,7 @@ def canny(image):
 	# Reduce Noise, convolving with a blur of 5x5 kernel
 	blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-	# Apply Canny function
+	# Apply Canny Edge Detection
 	canny = cv2.Canny(blur, 50, 150)
 
 	return canny
@@ -30,12 +27,63 @@ def region_of_interest(image):
 
 	return masked_image
 
+def display_lines(image, lines):
+	line_image = np.zeros_like(image)
+
+	if lines is not None:
+		for line in lines:
+			# print (line)
+			# x1, y1, x2, y2 = line.reshape(4)
+			x1, y1, x2, y2 = line
+			cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+	return line_image
+
+def make_coordinates(image, line_parameters):
+	slope, intercept = line_parameters
+
+	y1 = image.shape[0]
+	y2 = int(y1*(3/5))
+	x1 = int((y1 - intercept)/slope)
+	x2 = int((y2 - intercept)/slope)
+	return np.array([x1, y1, x2, y2])
+
+def average_slope_intercept(image, lines):
+	left_fit = []
+	right_fit = []
+
+	for line in lines:
+		x1, y1, x2, y2 = line.reshape(4)
+		parameters = np.polyfit((x1, x2), (y1, y2), 1) # fit polyomial of degree 1
+		slope = parameters[0]
+		intercept = parameters[1]
+		# lines on left have negative slope while on right have positive
+		if slope < 0:
+			left_fit.append((slope, intercept))
+		else:
+			right_fit.append((slope, intercept))
+	left_fit_average = np.average(left_fit, axis=0)
+	right_fit_average = np.average(right_fit, axis=0)
+
+	left_line = make_coordinates(image, left_fit_average)
+	right_line = make_coordinates(image, right_fit_average)
+
+	return np.array([left_line, right_line])
+
+
+
 image = cv2.imread('2.1_test_image.jpg')
 # print (type(image))
 lane_image = np.copy(image)
 canny_image = canny(lane_image)
 cropped_image = region_of_interest(canny_image)
-
-# show image
-cv2.imshow('result', cropped_image)
+# bin size, rho and theta, threshold
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, averaged_lines)
+# blend line image with original image
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('result', combo_image)
 cv2.waitKey(0)
+
+
